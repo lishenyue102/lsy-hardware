eaf596d8eff9ce85611d5b9743e8800c051c12c5
lishenyue <1916518720@qq.com>
2021-03-02 20:21:37 +0800

    first commit

---
+++ src/test/java/com/lsy/hardware/LsyHardwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.hardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsyHardwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
