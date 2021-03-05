c7bb503c3528e2e9e52eb2937d17f99563c3b7b0
lishenyue <1916518720@qq.com>
2021-03-05 16:25:30 +0800

    nacos动态配置完成

--- lsy-hardware-service/src/main/java/com/lsy/hardware/service/LsyHardwareServiceApplication.java
+++ lsy-hardware-service/src/main/java/com/lsy/hardware/service/LsyHardwareServiceApplication.java
@@ -1,10 +1,9 @@
 package com.lsy.hardware.service;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @MapperScan(value = {"com.lsy.hardware.**.dao"})
 @ImportResource(locations = {"classpath:config/spring.xml"})
-@NacosPropertySource(dataId = "example", autoRefreshed = true)
+@EnableDiscoveryClient
 public class LsyHardwareServiceApplication {
 
     public static void main(String[] args) {
--- lsy-hardware-service/src/main/java/com/lsy/hardware/service/TestController.java
+++ lsy-hardware-service/src/main/java/com/lsy/hardware/service/TestController.java
@@ -1,19 +1,23 @@
 package com.lsy.hardware.service;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *
+ *
  * @author lishenyue Created on 2021/3/2 21:36
  * @version 1.0
  */
 @RestController
 @RequestMapping("/config")
+@RefreshScope // 配置自动刷新
 public class TestController {
 
-    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    @Value("${useLocalCache}")
     private boolean useLocalCache;
 
 
