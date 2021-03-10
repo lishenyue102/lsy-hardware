4f4f6573a0efb9336c63234dfef1733e98c6b398
lishenyue <1916518720@qq.com>
2021-03-10 23:49:09 +0800

    1

--- lsy-hardware-web/src/main/java/com/lsy/hardware/web/configuration/HttpSessionConfig.java
+++ lsy-hardware-web/src/main/java/com/lsy/hardware/web/configuration/HttpSessionConfig.java
@@ -1,6 +1,7 @@
 package com.lsy.hardware.web.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
@@ -28,8 +30,6 @@ public class HttpSessionConfig {
         return new LettuceConnectionFactory();
     }
 
-
-
     /**
      * httpSession的事件监听，改用session提供的会话注册表
      *
@@ -38,5 +38,22 @@ public class HttpSessionConfig {
     @Bean
     public HttpSessionEventPublisher httpSessionEventPublisher(){
         return new HttpSessionEventPublisher();
+    }
+
+    /**
+     * findByIndexNameAndIndexValue 根据用户名反查该用户在线session集合
+     */
+    @Autowired
+    private FindByIndexNameSessionRepository sessionRepository;
+
+    /**
+     * SpringSessionBackedSessionRegistry是session为Spring Security提供的
+     * 用于在集群环境下控制会话并发的会话注册表实现类
+     *
+     * @return SpringSessionBackedSessionRegistry
+     */
+    @Bean
+    public SpringSessionBackedSessionRegistry sessionRegistry(){
+        return new SpringSessionBackedSessionRegistry(sessionRepository);
     }
 }
--- lsy-hardware-web/src/main/java/com/lsy/hardware/web/configuration/WebSecurityConfig.java
+++ lsy-hardware-web/src/main/java/com/lsy/hardware/web/configuration/WebSecurityConfig.java
@@ -1,13 +1,16 @@
 package com.lsy.hardware.web.configuration;
 
 import com.lsy.hardware.web.login.LsyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 import javax.annotation.Resource;
@@ -22,8 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private LsyUserDetailsService LsyUserDetailsService;
 
-//    @Resource
-//    private SpringSessionBackedSessionRegistry redisSessionRegistry;
+    @Resource
+    private SpringSessionBackedSessionRegistry redisSessionRegistry;
 
 
     @Override
@@ -35,11 +38,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
-//                .and()
-//                .sessionManagement()
-//                .maximumSessions(1)
-//                .sessionRegistry(redisSessionRegistry);
+                .formLogin()
+                .and()
+                .sessionManagement()
+                .maximumSessions(1)
+                .sessionRegistry(redisSessionRegistry);
     }
 
     @Override
@@ -48,20 +51,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(LsyUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    /**
-     * findByIndexNameAndIndexValue 根据用户名反查该用户在线session集合
-     */
-    @Resource
-    private FindByIndexNameSessionRepository findByIndexNameSessionRepository;
 
-    /**
-     * SpringSessionBackedSessionRegistry是session为Spring Security提供的
-     * 用于在集群环境下控制会话并发的会话注册表实现类
-     *
-     * @return SpringSessionBackedSessionRegistry
-     */
-    @Bean
-    public SpringSessionBackedSessionRegistry sessionRegistry(){
-        return new SpringSessionBackedSessionRegistry(findByIndexNameSessionRepository);
-    }
 }
